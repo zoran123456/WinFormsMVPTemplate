ed44b43f1a587217275442f25504838a317dddfe
Zoran Bošnjak <zbosnjak@iolap.com>
2019-10-30 10:21:48 +0100

    Add Initial Commit

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace WinFormsMVPTemplate
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
         /// Required designer variable.
@@ -28,10 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(599, 379);
+            this.Name = "MainForm";
+            this.Text = "Main Form";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
